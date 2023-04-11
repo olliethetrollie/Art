b29ba4d9d396ac64c8a41c85f7a05b4889f68e95
Oliver <111297871+olliethetrollie@users.noreply.github.com>
2023-04-11 09:03:41 -0600

    Added more mouse listeners

--- src/artist/view/ArtPanel.java
+++ src/artist/view/ArtPanel.java
@@ -24,7 +24,7 @@ public class ArtPanel extends JPanel
 		this.layout = new SpringLayout();
 		this.buttonPanel = new JPanel();
 		this.saveButton = new JButton();
-		this.demoLabel = new JLabel("Text :3");
+		this.demoLabel = new JLabel("");
 		this.canvas = new CanvasPanel(app);
 		
 		setupPanel();
@@ -38,6 +38,7 @@ public class ArtPanel extends JPanel
 		this.setBackground(Color.DARK_GRAY);
 		this.add(demoLabel);
 		this.add(canvas);
+		demoLabel.setBackground(Color.WHITE);
 	}
 	
 	public void setupListeners()
@@ -59,6 +60,38 @@ public class ArtPanel extends JPanel
 				}
 				
 		});
+		
+		canvas.addMouseListener(new MouseListener()
+				{
+					public void mouseClicked(MouseEvent click)
+					{
+						
+					}
+					
+					public void mousePressed(MouseEvent press)
+					{
+						
+					}
+					
+					public void mouseReleased(MouseEvent release)
+					{
+						canvas.resetPoint();
+					}
+					
+					public void mouseEntered(MouseEvent enter)
+					{
+						
+					}
+					
+					public void mouseExited(MouseEvent exit)
+					{
+						canvas.resetPoint();
+					}
+				}
+				
+				
+				
+				);
 	}
 	
 	public void setupLayout()
